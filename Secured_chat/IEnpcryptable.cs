04187403ec58ebc79f0cc03705f2b43707c7c35b
celeouet <eleouet.clement@outlook.fr>
2022-12-05 21:06:19 +0100

    pouet

--- Secured_chat/IEnpcryptable.cs
+++ Secured_chat/IEnpcryptable.cs
@@ -13,19 +13,20 @@ namespace Secured_chat
 	/// <summary>
 	/// Set of tools to encrypt and decrypt messages
 	/// </summary>
-	public interface IEnpcryptable<T, U>
+	public interface IEnpcryptable<T>
 	{
 		/// <summary>
 		/// Encrypt the message using RSA algorithm and small integers
 		/// </summary>
 		/// <param name="m">structure containing the message to encrypt</param>
 		/// <param name="key">1st factor of the public key</param>
-		void Encrypt(T m, RSAKey key);
+		void Encrypt(RSAKey key);
+
 		/// <summary>
 		/// Decrypt the message using RSA algorithm and small integers
 		/// </summary>
 		/// <param name="m"></param>
 		/// <param name="key"></param>
-		void Decrypt(T m, RSAKey key);
+		void Decrypt(RSAKey key);
 	}
 }
---
+++ Secured_chat/SessionData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secured_chat
+{
+    internal class SessionData
+    {
+        RSa
+        static SessionData instance;
+        public SessionData GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new SessionData();
+            }
+            return instance;
+        }
+
+
+    }
+}
